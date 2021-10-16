e66f6bee9cf8d75c3cb6a8593a988712728b2911
Paulo Duque <paulo.duque@terra.com.br>
2021-10-16 09:50:16 -0300

    Criação do Script2 com a estrutura Switch

---
+++ Aula 03/js/script2.js
@@ -0,0 +1,33 @@
+
+var cor = prompt("Digite uma cor:\nVermelho,Verde,Azul,Amarelo,Preto,Cinza");
+var fundo="";
+/* Transformar em 
+toLowerCase() = minúsculo ou 
+toUpperCase() = maiúsculas
+*/
+cor = cor.toLocaleLowerCase();
+
+switch(cor){
+    case"vermelho";
+        fundo="red";
+        break;
+    case"verde";
+        fundo="green";
+        break; 
+    case"azul";
+        fundo="blue";
+        break;   
+    case"amarelo";
+        fundo="yellow";
+        break;
+    case"preto";
+        fundo="black";
+        break; 
+    case"cinza";
+        fundo="gray";
+        break;  
+    default:
+        fundo="white";
+        break; 
+}
+document.body.style.backgroundColor=fundo; 
